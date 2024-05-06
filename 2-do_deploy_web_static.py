8cb83efcb17070850126990efd0b1958c570d9d2
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2024-05-06 15:47:33 -0700

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["100.25.33.68", "54.146.67.224"]
 env.user = "ubuntu"
 
 
